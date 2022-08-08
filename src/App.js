71064aee59d57d3645edf3c5944e3e192bbb4baf
Jay <jaysojitra1011@gmail.com>
2022-08-08 17:53:59 +0530

    cryptoapp pushed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import { Layout, Space, Typography } from 'antd';
+import { Link, Route, Routes } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Exchanges from './components/Exchanges';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import News from './components/News';
+import CryptoDetails from './components/CryptoDetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved<br />
+            <Space>
+              <Link to='/'>Home</Link>
+              <Link to='/exchanges'>Exchanges</Link>
+              <Link to='/news'>News</Link>
+            </Space>
+          </Typography.Title>
+        </div>
+      </div >
+
+    </div >
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import { BulbOutlined, FundOutlined, HomeOutlined, MenuOutlined, MoneyCollectOutlined, UserOutlined, } from '@ant-design/icons';
+import { Avatar, Typography, Menu, Button } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import icon from '../images/cryptocurrency.png';
+
+const Navbar = () => {
+
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null)
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(Window.innerWidth)
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+
+    }, []);
+
+    useEffect(() => {
+        if (screenSize < 767) {
+            setActiveMenu(false)
+        }
+        else {
+            setActiveMenu(true)
+        }
+    }, [screenSize])
+
+    return (
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className='logo'>
+                    <Link to='/'>Cryptoverse</Link>
+                </Typography.Title>
+                <Button className='menu-control-container' onClick={() => setActiveMenu(!activeMenu)}>
+                    <MenuOutlined />
+                </Button>
+            </div>
+            {activeMenu && (
+
+                <Menu theme='dark'>
+                    <Menu.Item icon={<HomeOutlined />}>
+                        <Link to='/'>Home</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<FundOutlined />}>
+                        <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<MoneyCollectOutlined />}>
+                        <Link to='/exchanges'>Exachanges</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<BulbOutlined />}>
+                        <Link to='/news'>News</Link>
+                    </Menu.Item>
+                </Menu>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
+
